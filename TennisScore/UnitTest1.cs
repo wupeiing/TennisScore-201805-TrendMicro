da22c8ffce7f13173e04ed07a855ca5ad48d5b6f
hatelove <jay.hatelove@gmail.com>
2018-03-27 14:36:37 +0800

    Add project files.

---
+++ TennisScore/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+
+namespace TennisScore
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Love_All()
+        {
+            var gameId = 1;
+
+            IRepository<Game> repo = Substitute.For<IRepository<Game>>();
+            repo.GetGame(gameId).Returns(new Game {Id = gameId, FirstPlayerScore = 0, SecondPlayerScore = 0});
+
+            TennisGame tennisGame = new TennisGame(repo);
+
+            var scoreResult = tennisGame.ScoreResult(gameId);
+            Assert.AreEqual("Love All", scoreResult);
+        }
+    }
+}
