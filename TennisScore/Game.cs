d4826dcd859d4fe86cebc61b683b688d8d7c6760
hatelove <jay.hatelove@gmail.com>
2018-03-28 12:21:22 +0800

    refactor accessiability

--- TennisScore/Game.cs
+++ TennisScore/Game.cs
@@ -5,44 +5,10 @@ namespace TennisScore
 {
     public class Game
     {
-        public int SecondPlayerScore { get; set; }
-        public int FirstPlayerScore { get; set; }
-        public int Id { get; set; }
-        public string FirstPlayerName { get; set; }
-        public string SecondPlayerName { get; set; }
+        private const string AdvanceScoreResult = "Adv";
+        private const string Deuce = "Deuce";
 
-        public bool IsDifferentScore()
-        {
-            return FirstPlayerScore != SecondPlayerScore;
-        }
-
-        public bool IsDeuce()
-        {
-            var isSameScore = !IsDifferentScore();
-            return FirstPlayerScore >= 3 && isSameScore;
-        }
-
-        public string AdvPlayer()
-        {
-            return FirstPlayerScore > SecondPlayerScore
-                ? FirstPlayerName
-                : SecondPlayerName;
-        }
-
-        public bool IsAdvance()
-        {
-            return Math.Abs(FirstPlayerScore - SecondPlayerScore) == 1;
-        }
-
-        public bool IsReadyForWin()
-        {
-            return FirstPlayerScore > 3 || SecondPlayerScore > 3;
-        }
-
-        public const string Deuce = "Deuce";
-        public const string AdvanceScoreResult = "Adv";
-
-        public static Dictionary<int, string> _scoreLookup = new Dictionary<int, string>
+        private static Dictionary<int, string> _scoreLookup = new Dictionary<int, string>
         {
             {0, "Love"},
             {1, "Fifteen"},
@@ -50,36 +16,70 @@ namespace TennisScore
             {3, "Forty"},
         };
 
-        public string AdvScore()
-        {
-            return AdvPlayer() + " " + Game.AdvanceScoreResult;
-        }
-
-        public string WinScore()
-        {
-            return AdvPlayer() + " " + "Win";
-        }
-
-        public string AdvState()
-        {
-            return IsAdvance() ? AdvScore() : WinScore();
-        }
-
-        public string ScoreLookup()
-        {
-            return Game._scoreLookup[this.FirstPlayerScore] + " " + Game._scoreLookup[this.SecondPlayerScore];
-        }
-
-        public string SameScore()
-        {
-            return Game._scoreLookup[this.FirstPlayerScore] + " All";
-        }
+        public string FirstPlayerName { get; set; }
+        public int FirstPlayerScore { get; set; }
+        public int Id { get; set; }
+        public string SecondPlayerName { get; set; }
+        public int SecondPlayerScore { get; set; }
 
         public string ScoreResult()
         {
             return IsDifferentScore()
                 ? (IsReadyForWin() ? AdvState() : ScoreLookup())
                 : (IsDeuce() ? Game.Deuce : SameScore());
+        }
+
+        private string AdvPlayer()
+        {
+            return FirstPlayerScore > SecondPlayerScore
+                ? FirstPlayerName
+                : SecondPlayerName;
+        }
+
+        private string AdvScore()
+        {
+            return AdvPlayer() + " " + Game.AdvanceScoreResult;
+        }
+
+        private string AdvState()
+        {
+            return IsAdvance() ? AdvScore() : WinScore();
+        }
+
+        private bool IsAdvance()
+        {
+            return Math.Abs(FirstPlayerScore - SecondPlayerScore) == 1;
+        }
+
+        private bool IsDeuce()
+        {
+            var isSameScore = !IsDifferentScore();
+            return FirstPlayerScore >= 3 && isSameScore;
+        }
+
+        private bool IsDifferentScore()
+        {
+            return FirstPlayerScore != SecondPlayerScore;
+        }
+
+        private bool IsReadyForWin()
+        {
+            return FirstPlayerScore > 3 || SecondPlayerScore > 3;
+        }
+
+        private string SameScore()
+        {
+            return Game._scoreLookup[this.FirstPlayerScore] + " All";
+        }
+
+        private string ScoreLookup()
+        {
+            return Game._scoreLookup[this.FirstPlayerScore] + " " + Game._scoreLookup[this.SecondPlayerScore];
+        }
+
+        private string WinScore()
+        {
+            return AdvPlayer() + " " + "Win";
         }
     }
 }
